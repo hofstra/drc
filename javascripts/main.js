f6f26e107af6567ddcd9fb9bc4e1efb3346ec7f7
Andy Stuhl <andykstuhl@gmail.com>
2017-03-23 15:03:33 -0500

    initial nav bar update

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   var menuToggle = $('#js-navigation-mobile-menu').unbind();
   $('#js-navigation-menu').removeClass("show");
-  
+
   menuToggle.on('click', function(e) {
     e.preventDefault();
     $('#js-navigation-menu').slideToggle(function(){
@@ -9,5 +9,14 @@ $(document).ready(function() {
         $('#js-navigation-menu').removeAttr('style');
       }
     });
+  });
+
+  $("ul.navigation-menu > li.nav-link").hover(function() {
+    console.log("hover");
+    $(this).addClass("nav-hover");
+    $(this).find("ul.nav-nested").removeClass("hidden");
+  }, function() {
+    $(this).removeClass("nav-hover");
+    $(this).find("ul.nav-nested").addClass("hidden");
   });
 });
